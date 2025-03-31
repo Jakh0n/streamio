db0a5da1dab059d827ea45bddd04da7777b41203
Jakhongirbek <jakhonyokubjonov@gmail.com>
2025-03-31 13:18:45 +0900

    User Profile finish

--- app/(root)/u/[label]/page.tsx
+++ app/(root)/u/[label]/page.tsx
@@ -1,5 +1,9 @@
 import { getUserByUsername } from '@/actions/user.action'
 import UserAvatar from '@/components/shared/user-avatar'
+import { Button } from '@/components/ui/button'
+import { currentUser } from '@clerk/nextjs/server'
+import Link from 'next/link'
+import SubscribeBtn from '../../_components/subscribe-btn'
 
 interface UsernamePageProps {
 	params: Promise<{ label: string }>
@@ -10,6 +14,7 @@ const UserNamePage = async ({ params }: UsernamePageProps) => {
 	const response = await getUserByUsername({ label })
 	console.log('Response:', response)
 	const user = response?.data?.data?.user
+	const self = await currentUser()
 	console.log('User:', user)
 
 	return (
@@ -19,16 +24,64 @@ const UserNamePage = async ({ params }: UsernamePageProps) => {
 					Banner Image
 				</h1>
 			</div>
-			<div className='w-full md:w-1/2'>
-				<div className='flex items-center gap-2'>
+			<div className='w-full lg:w-1/2 mt-4'>
+				<div className='flex items-start space-x-4'>
 					{user && (
-						<UserAvatar avatar={user.avatar} label={user.label} size={'lg'} />
+						<UserAvatar avatar={user?.avatar} label={user?.label} size={'xl'} />
 					)}
-					<div className='flex items-center space-x-2 flex-col'>
-						<h1 className='text-xl lg:text-2xl font-medium lg:font-bold'>
-							{user?.label}
-						</h1>
-						<p className='text-sm text-muted-foreground'>{user?.label}</p>
+
+					<div className='flex flex-col space-y-0'>
+						<h2 className='font-space_grotesk text-xl lg:text-2xl font-bold'>
+							{user?.fullName || 'No name provided'}
+						</h2>
+
+						<div className='flex items-center gap-x-1 lg:gap-x-2'>
+							<p className='text-sm lg:text-base'>@{user?.label}</p>
+							<div className='size-1  rounded-full bg-muted-foreground' />
+							<p className='text-muted-foreground text-sm lg:text-base'>
+								12 subscribers
+							</p>
+							<div className='size-1 rounded-full bg-muted-foreground' />
+							<p className='text-muted-foreground text-sm lg:text-base'>
+								10 videos
+							</p>
+						</div>
+
+						<p className='line-clamp-2 leading-4 text-sm lg:text-base text-muted-foreground'>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+							Reiciendis, nobis, provident beatae omnis recusandae iusto quia
+							nisi cum exercitationem enim vel rem ipsam illum ex nihil! Illum
+							tempora sequi a.
+						</p>
+
+						{self && user && self.username === user.label ? (
+							<div className='flex items-center space-x-3'>
+								<Button
+									variant={'outline'}
+									size={'sm'}
+									className='rounded-full mt-4 '
+									asChild
+								>
+									<Link href={'/dashboard/settings'}>
+										<h1 className='text-sm lg:text-base'>Customize channel</h1>
+									</Link>
+								</Button>
+								<Button
+									variant={'secondary'}
+									size={'sm'}
+									className='rounded-full mt-4 '
+									asChild
+								>
+									<Link href={'/dashboard/videos'}>
+										<h1 className='text-sm lg:text-base'>Manage videos</h1>
+									</Link>
+								</Button>
+							</div>
+						) : (
+							<div>
+								<SubscribeBtn isFollowing={false} />
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
